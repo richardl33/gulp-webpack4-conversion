16da98bfbca4d5844c1bab1cc29ff7e022ffbded
richardl33 <rich.leeroy@gmail.com>
2019-01-20 22:06:38 -0500

    optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,21 @@ module.exports = {
          {
             test: /\.html$/,
             use: [{ loader: "html-loader", options: { minimize: true } }]
+         },
+         {
+            test: /\.(png|jpe?g)/i,
+            use: [
+               {
+                  loader: "url-loader",
+                  options: {
+                  name: "./img/[name].[ext]",
+                  limit: 10000
+               }
+               },
+               {
+                  loader: "img-loader"
+               }
+            ]
          }
       ]
    },
