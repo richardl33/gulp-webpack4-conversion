db2b6c879aa81216ea9612889cff13d17522fb41
richardl33 <rich.leeroy@gmail.com>
2019-01-21 00:14:32 -0500

    add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,14 @@ module.exports = {
                "postcss-loader",
                "sass-loader"
             ]
-         }
+         },
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader"
+            }
+          }
       ]
    },
    plugins: [
